00452d0ea4b97b6c5e2e333424ac3251b030213f
Edson Junior <edsonkjr@gmail.com>
2019-04-20 21:51:39 -0300

    Inserindo uma nova Categotia com POST

--- src/main/java/com/edson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edson/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.edson.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.edson.cursomc.domain.Categoria;
 import com.edson.cursomc.services.CategoriaService;
@@ -23,5 +27,15 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	//Receber uma categoria no formato Json e inserir no banco de dados.
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.
+		fromCurrentRequest().path("/{id}").
+		buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
